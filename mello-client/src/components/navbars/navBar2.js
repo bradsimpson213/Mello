e91a82799fb243ce6bd9c4b01b16946601ab3c6b
Brad Simpson <bradsimpson@icloud.com>
2020-07-24 10:18:19 -0400

    play music toggle

--- mello-client/src/components/navbars/navBar2.js
+++ mello-client/src/components/navbars/navBar2.js
@@ -1,7 +1,9 @@
 import React, { useContext } from 'react';
 import { Redirect } from 'react-router-dom';
+import useToggle from '../hooks/useToggle';
 import { FiHome } from 'react-icons/fi';
 import { FaTrello } from 'react-icons/fa';
+import { GoMute, GoUnmute } from 'react-icons/go';
 import styles from './NavBar2.module.css';
 import { Avatar, Button, Icon, Input, InputGroup, InputRightElement, 
     Menu, MenuButton, MenuList, MenuItem } from "@chakra-ui/core";
@@ -9,6 +11,14 @@ import appContext from '../../Context'
 
 
 const NavBar2 = () => {
+    const [icon, toggleIcon] = useToggle(true);
+    const myAudio = document.getElementById('myAudio')
+
+    const toggleAudio = () => {
+      toggleIcon();
+      return myAudio.paused ? myAudio.play() : myAudio.pause();
+    };
+
     
     const { user } = useContext(appContext);
 
@@ -18,7 +28,12 @@ const NavBar2 = () => {
           className={styles.homeIcon}
           onClick={<Redirect to="/boards" />}
         />
-        <audio autoplay="true" loop="true">
+        {icon ? (
+          <GoUnmute className={styles.music} onClick={toggleAudio} />
+        ) : (
+          <GoMute className={styles.music} onClick={toggleAudio} />
+        )}
+        <audio id="myAudio" autoplay="true" loop="true">
           <source
             src="https://mello-music.s3.amazonaws.com/2020-06-18_-_Serenity_-_www.FesliyanStudios.com_David_Renda.mp3"
             type="audio/mpeg"
