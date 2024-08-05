9c4ab73720fd337699a20fa8fc9adda0d52dd827
bradsimpson213 <bradsimpson@icloud.com>
2024-08-04 22:25:58 -0400

    added seed command import

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,5 +1,12 @@
 from flask_sqlalchemy import SQLAlchemy
 import os
+from .boards import Board
+from .cards import Card
+from .checklists import Checklist
+from .comments import Comment
+from .lists import List
+from .teams import Team
+from .users import User
 
 environment = os.getenv("FLASK_ENV")
 SCHEMA = os.environ.get("SCHEMA")
